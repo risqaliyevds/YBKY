f90f75c3ded07b1fce894c273026711c0b26f80a
Muradjon Riskaliev <risqaliyevds@gmail.com>
2023-06-19 16:59:23 +0500

    deleted

--- Interview 2 - 14.06.2023/code.py
+++
@@ -1,7 +0,0 @@
-def calc_drone_min_energy(route):
-    min_ = max_ = route[0][-1]
-    for i in range(0, len(route)):
-        if route[i][-1] > max_:
-            max_ = route[i][-1]
-
-    return max_ - min_
